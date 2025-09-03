aeb1cea8342d6a8106064b96b7849bb75cab4f35
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 14:31:01 +0530

    add jdbc user details manager

--- src/main/java/com/tw/mvcSecurity/security/MVCSecurity.java
+++ src/main/java/com/tw/mvcSecurity/security/MVCSecurity.java
@@ -4,10 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class MVCSecurity {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
