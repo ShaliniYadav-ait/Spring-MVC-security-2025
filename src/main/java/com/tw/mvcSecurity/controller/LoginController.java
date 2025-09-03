9e88c144deaa702799b0888414ef6080b51a5140
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 10:01:46 +0530

    add bootstrap login form

--- src/main/java/com/tw/mvcSecurity/controller/LoginController.java
+++ src/main/java/com/tw/mvcSecurity/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class loginController {
+public class LoginController {
 
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
-        return "plain-login";
+        return "fancy-login";
     }
-    
+
 }
